b0dc5be899fcf3dbddbe402ae640b3d755663564
Octavio <Oasosaf@gmail.com>
2017-08-16 09:10:31 -0500

    add Restaurant() class and an instance

--- 9.1.py
+++ 9.1.py
@@ -1,2 +1,18 @@
 #Class for Restaurant. __init__() method should have attributes: 
-#restaurant_name and cuisine_type. 
+#restaurant_name and cuisine_type.
+
+class Restaurant():
+    def __init__(self,restaurant_name,cuisine_type):
+        self.restaurant_name = restaurant_name
+        self.cuisine_type = cuisine_type
+
+    def describe_restaurant(self):
+        print('\n',self.restaurant_name.title(), 'is a', self.cuisine_type.title())
+        print('restaurant with in house butchers to customize your cuts')
+
+    def open_restaurant():
+        print(self.restaurant_name.title(), 'is now open!')
+
+myFamilyRestaurant = Restaurant('El_Torito','mexican_as_fuck')
+myFamilyRestaurant.describe_restaurant()
+myFamilyRestaurant.open_restaurant()
