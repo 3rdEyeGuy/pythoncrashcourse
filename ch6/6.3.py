afea63b7c93a57a8a6b6b7405181d99a621440c5
Octavio <Oasosaf@gmail.com>
2017-09-03 13:10:31 -0500

    add 5 key words to a dict with values + print key-value pairs

---
+++ ch6/6.3.py
@@ -0,0 +1,13 @@
+codingDict = dict()
+
+codingDict["if"] = "Performs a loop \"if\" the statement is true."
+codingDict["for"] = "Performs a loop a given amount of times."
+codingDict["while"] = "Performs a loop an unknown amount of times until the loop statement is false."
+codingDict["return"] = "Placed at the end of a function. Used to return a value\
+ for the function called."
+codingDict["print"] = "Prints a string literal."
+
+for i in codingDict:
+    print('\n'+i+':',codingDict[i])
+
+print()
