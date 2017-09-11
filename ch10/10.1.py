0f8b284ba51d70908e114e59fdb6227b34bf5843
Octavio <Oasosaf@gmail.com>
2017-09-10 20:52:30 -0500

    add correct loops

--- ch10/10.1.py
+++ ch10/10.1.py
@@ -1,24 +1,19 @@
 with open("learning_python.txt") as file_object:
-    lines = file_object.readlines()
+    content = file_object.read()
 
-    print()
-
-    for line in lines:
-        print(line.rstrip())
-
-    print()
+    print('\n' + "Reading in entire file")
+    print(content)
 
 with open("learning_python.txt") as file_object:
+    print("Loop over lines")
     for line in file_object:
         print(line.rstrip())
 
     print()
 
 with open("learning_python.txt") as file_object:
-    strings = ''
+    print("Loop over list")
+    lines = file_object.readlines()
     for line in lines:
-        strings += line.rstrip()
-
-    for sentance in range(len(strings)):
-        print(strings[sentance])
-    print(len(strings))
+        print(line.rstrip())
+    print()
