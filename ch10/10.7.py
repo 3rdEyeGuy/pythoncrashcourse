314fe9f355facdf7799650d90b685aa53924f8b2
Octavio <Oasosaf@gmail.com>
2017-09-17 13:49:38 -0500

    add isinstance() rather than isdigit() for while loop statement

--- ch10/10.7.py
+++ ch10/10.7.py
@@ -12,12 +12,15 @@ try:
 
 except:
     print("Error: Please enter a number.")
+    num1 = input("Enter the 1st number: ")
+    num2 = input("Enter the 2nd number: ")
 
-    while num1.isdigit() == False | num2.isdigit() == False:
-        num1 = int(input("Enter the 1st number: "))
-        num2 = int(input("Enter the 2nd number: "))
-        numsum = num1 + num2
-        print("The sum of", str(num1), "and", str(num2), "is", str(numsum) + ".")
+    while isinstance(num1, int) != True | isinstance(num2, int) != True:
+        num1 = input("Enter the 1st number: ")
+        num2 = input("Enter the 2nd number: ")
+
+    numsum = int(num1) + int(num2)
+    print("The sum of", str(num1), "and", str(num2), "is", str(numsum) + ".")
 
 #Prints sum of two numbers if 2 numbers were entered.
 else:
