3116355ab0cea3f4007ac5e94e6d0ca70ba0dff4
Octavio <Oasosaf@gmail.com>
2017-09-17 13:59:28 -0500

    revert to 10.6.py code

--- ch10/10.7.py
+++ ch10/10.7.py
@@ -1,30 +1,16 @@
 """Prompts for 2 numbers to sum and prints result"""
 
 print("Enter two numbers to sum.") 
-num1 = input("Enter the 1st number: ")
-num2 = input("Enter the 2nd number: ")
 
 #Exception in case user fails to enter a number
 try:
-    num1 = int(num1)
-    num2 = int(num2)
-    numsum = num1 + num2
-
+    num1 = int(input("Enter the 1st number: "))
+    num2 = int(input("Enter the 2nd number: "))
 except:
     print("Error: Please enter a number.")
-    num1 = input("Enter the 1st number: ")
-    num2 = input("Enter the 2nd number: ")
-
-    while isinstance(num1, int) != True | isinstance(num2, int) != True:
-        num1 = input("Enter the 1st number: ")
-        num2 = input("Enter the 2nd number: ")
-
-    numsum = int(num1) + int(num2)
-    print("The sum of", str(num1), "and", str(num2), "is", str(numsum) + ".")
-
 #Prints sum of two numbers if 2 numbers were entered.
 else:
-    
+    numsum = num1 + num2
     print("The sum of", str(num1), "and", str(num2), "is", str(numsum) + ".")
 
 
