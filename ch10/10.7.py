09dc6d578bb1d99cbb6966108f026c8b2784f6be
Octavio <Oasosaf@gmail.com>
2017-09-17 14:10:34 -0500

    add infinite while loop (while True:) to loop exception block

--- ch10/10.7.py
+++ ch10/10.7.py
@@ -1,16 +1,26 @@
 """Prompts for 2 numbers to sum and prints result"""
 
-print("Enter two numbers to sum.") 
+print("Enter two numbers to sum. Enter 'q' to quit.") 
 
-#Exception in case user fails to enter a number
-try:
-    num1 = int(input("Enter the 1st number: "))
-    num2 = int(input("Enter the 2nd number: "))
-except:
-    print("Error: Please enter a number.")
-#Prints sum of two numbers if 2 numbers were entered.
-else:
-    numsum = num1 + num2
-    print("The sum of", str(num1), "and", str(num2), "is", str(numsum) + ".")
+#while True is True loop the exception block (infinite loop).
+while True:
+    #Exception in case user fails to enter a number
+    try:
+        num1 = input("Enter the 1st number: ")
+        if num1 == 'q':
+            break
+        num1 = int(num1)
+
+        num2 = input("Enter the 2nd number: ")
+        if num2 == 'q':
+            break
+        num2 = int(num2)
+        
+    except:
+        print("Error: Please enter a number.")
+    #Prints sum of two numbers if 2 numbers were entered.
+    else:
+        numsum = num1 + num2
+        print("The sum of", str(num1), "and", str(num2), "is", str(numsum) + ".")
 
 
