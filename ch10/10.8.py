80853be117df3857aaf1e09c04900a8c5f31a95d
Octavio <Oasosaf@gmail.com>
2017-09-17 16:19:26 -0500

    add function for opening files and printing contents

--- ch10/10.8.py
+++ ch10/10.8.py
@@ -1,14 +1,18 @@
+"""Opens files and reads their contents"""
+
+#Function to open and print files
+def printFile(file_name):
+    #Exception block
+    try:
+        with open(file_name) as fObj:
+            lines = fObj.readlines()
+            for line in lines:
+                print(line.rstrip())
+    except:
+        print("The file '"+ file_name + "' is missing.")
+
+#loop to use above function to print the files' contents.
 filenames = ['cats.txt','dogs.txt']
-
-try:
-    for filename in filenames:
-        printFile(filename)
-except:
-    print("The file '"+ filename + "' is missing.")
-
-def printFile(file_names):
-    with open(file_names) as fObj:
-        lines = fObj.readlines()
-        for line in lines:
-            print(line.rstrip())
-
+for filename in filenames:
+    print(filename + ':')
+    printFile(filename)
