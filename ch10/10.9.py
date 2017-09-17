bc8b8342c5fdf08050bdd9ecd809fc1d8a5558bb
Octavio <Oasosaf@gmail.com>
2017-09-17 16:26:11 -0500

    add 'pass' to silently fail if file is not found

--- ch10/10.9.py
+++ ch10/10.9.py
@@ -6,13 +6,13 @@ def printFile(file_name):
     try:
         with open(file_name) as fObj:
             lines = fObj.readlines()
+            print(file_name + ':')
             for line in lines:
                 print(line.rstrip())
     except:
-        print("The file '"+ file_name + "' is missing.")
+        pass
 
 #loop to use above function to print the files' contents.
 filenames = ['cats.txt','dogs.txt']
 for filename in filenames:
-    print(filename + ':')
     printFile(filename)
