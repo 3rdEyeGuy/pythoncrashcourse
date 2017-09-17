acccd19622468d909210fe6e99131a0710d2487e
Octavio <Oasosaf@gmail.com>
2017-09-17 13:28:26 -0500

    move int() conversion from input() call to try block

---
+++ ch10/10.7.py
@@ -0,0 +1,27 @@
+"""Prompts for 2 numbers to sum and prints result"""
+
+print("Enter two numbers to sum.") 
+num1 = input("Enter the 1st number: ")
+num2 = input("Enter the 2nd number: ")
+
+#Exception in case user fails to enter a number
+try:
+    num1 = int(num1)
+    num2 = int(num2)
+    numsum = num1 + num2
+
+except:
+    print("Error: Please enter a number.")
+
+    while num1 != int() & num2 != int():
+        num1 = int(input("Enter the 1st number: "))
+        num2 = int(input("Enter the 2nd number: "))
+        numsum = num1 + num2
+        print("The sum of", str(num1), "and", str(num2), "is", str(numsum) + ".")
+
+#Prints sum of two numbers if 2 numbers were entered.
+else:
+    
+    print("The sum of", str(num1), "and", str(num2), "is", str(numsum) + ".")
+
+
