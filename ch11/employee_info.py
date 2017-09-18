8aeb5804e4228f15c05cd3de343c4ba4e43b0584
Octavio <Oasosaf@gmail.com>
2017-09-18 13:10:53 -0500

    change name of 11.3.py

---
+++ ch11/employee_info.py
@@ -0,0 +1,12 @@
+class Employee():
+    """Stores first and last names as well as the salary"""
+
+    def __init__(self, first, last, salary):
+        """initializes first, last and salary"""
+        self.first = first
+        self.last = last
+        self.salary = salary
+
+    def give_raise(self, salRaise = 5000):
+        """default raise of $5000 but can be customized"""
+        self.salary = salary + salRaise
