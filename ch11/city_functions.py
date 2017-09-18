40fdfc87d601483c71c8587be6314b24d4b4d6eb
Octavio <Oasosaf@gmail.com>
2017-09-18 11:39:25 -0500

    correct title() placement and fix space error

--- ch11/city_functions.py
+++ ch11/city_functions.py
@@ -1,7 +1,7 @@
 def city_format(city, country, population = ''):
     """Formats the city and country in a single string"""
     if population:
-        cityCountry = city + ', ' + country + ' - ' + 'population' + population
+        cityCountry = city.title() + ', ' + country.title() + ' - ' + 'population ' + population
     else:
-        cityCountry = city + ', ' + country
-    return cityCountry.title()
+        cityCountry = city.title() + ', ' + country.title()
+    return cityCountry
