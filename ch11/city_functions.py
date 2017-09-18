ac8d6ece872fae6be62df22e7cea4d04d888a6ec
Octavio <Oasosaf@gmail.com>
2017-09-18 11:28:22 -0500

    add 3rd argument (population) for function

--- ch11/city_functions.py
+++ ch11/city_functions.py
@@ -1,4 +1,4 @@
-def city_format(city, country):
+def city_format(city, country, population):
     """Formats the city and country in a single string"""
-    cityCountry = city + ', ' + country
+    cityCountry = city + ', ' + country + ' - ' + 'population' + population
     return cityCountry.title()
